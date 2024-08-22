248c9fa51febe0017579c4f8d0bca0b7a8b170db
Piotr Kalota <piotr.kalota.dev@gmail.com>
2024-08-22 16:43:36 +0200

    Change author to models directory

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.file_utils import FileUtils
-from src.author.author import Author
-from src.author.collection import Collection
+from src.models.author import Author
+from src.models.collection import Collection
 from src.analysis.analysis import Analysis
 from src.analysis.analysis_visualization import AnalysisVisualization
 from src.settings import Secrets, Configuration
