ffb777b9e0d906b40a5c299b535b9ce748d73273
Piotr Kalota <piotr.kalota.dev@gmail.com>
2024-10-09 19:12:01 +0200

    Add print_author_chunks_results

--- src/classification/classification_data.py
+++ src/classification/classification_data.py
@@ -21,26 +21,20 @@ class LogisticRegressionResults:
 class LogisticRegressionResultsTransformer:
 
     @staticmethod
-    def print(logistic_regression_results: LogisticRegressionResults) -> str:
-        LogisticRegressionResultsTransformer._print_all_chunks_results(logistic_regression_results.all_chunks_binary_classification)
-        LogisticRegressionResultsTransformer._print_author_chunks_results(logistic_regression_results.authors_chunks_binary_classification)
-
-    @staticmethod
-    def _print_all_chunks_results(logistic_regression_data: LogisticClassificationData) -> str:
+    def print_all_chunks_results(logistic_regression_data: LogisticClassificationData) -> str:
         print(f"Cross-validation accuracy: {logistic_regression_data.cross_validation_accuracy}\n ---")
         print(f"Cross-validation accuracy per class:\n {logistic_regression_data.accuracy_per_class}\n ---")
-        print(f"Cross-validation accuracy per author:\n {logistic_regression_data.accuracy_per_author}\n ---")
+        print(f"Cross-validation accuracy per author:\n {logistic_regression_data.accuracy_per_author}\n")
 
     @staticmethod
-    def _print_author_chunks_results(logistic_regression_data: Dict[str, LogisticClassificationData]) -> str:
+    def print_author_chunks_results(logistic_regression_data: Dict[str, LogisticClassificationData]) -> str:
         df = pd.DataFrame()
+        total_accuracy = 0
         for author_name, results in logistic_regression_data.items():
-            df = df.append(
-                pd.DataFrame(
-                    {
-                        'author_name': author_name,
-                        'accuracy_per_class': results.accuracy_per_class
-                    }
-                ) 
-            )
-        print(df)
+            total_accuracy += results.cross_validation_accuracy
+            series = pd.Series([author_name, results.accuracy_per_class['llm'], results.accuracy_per_class['human']])
+            df = pd.concat([df, series.to_frame().T])
+        print(f"Average cross-validation accuracy: {total_accuracy / len(logistic_regression_data)}\n ---")
+        df.reset_index()
+        df.columns=['author_name', 'accuracy_llm', 'accuracy_human']
+        print(f"Cross-validation accuracy for each author PCA:\n {df}")
