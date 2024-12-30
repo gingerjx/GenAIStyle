228644f53b48d70d5f3ad115506e1583833c48b8
Piotr Kalota <piotr.kalota.dev@gmail.com>
2024-12-30 22:00:20 +0100

    Exclude chunk_id from training features

--- src/classification/common/pca_classification.py
+++ src/classification/common/pca_classification.py
@@ -7,7 +7,7 @@ class BaseClassification:
     @staticmethod
     def _transform_data_for_collection_classification(pca_analysis_results_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
         df = pca_analysis_results_data.copy()
-        df = df.drop(columns=['source_name', 'author_name'])
+        df = df.drop(columns=['source_name', 'author_name', 'chunk_id'])
         X = df.drop(columns=['collection_name'])
         y = df['collection_name']
         return X, y
